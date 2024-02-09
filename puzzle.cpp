d88894d8697ed67a53fd4107cd265706929b6c9c
Guillaume Cuadrado (GUCD) (Consultant) <gucd@oticonmedical.com>
2024-02-09 12:20:54 +0100

    Add puzzle.cpp puzzle.h in build flow

---
+++ puzzle.cpp
@@ -0,0 +1,8 @@
+#include "puzzle.h"
+
+int puzzle_2023_1(QString &result, const QString &input) {
+
+    result = input.at(0);
+
+    return 0;
+}
---
+++ puzzle.h
@@ -0,0 +1,8 @@
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+#include <QString>
+
+int puzzle_2023_1(QString &result, const QString &input);
+
+#endif // PUZZLE_H
